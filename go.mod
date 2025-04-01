71f7d554b71c5331c2accb7896a3260c2c85cde3
Farhan H <farhan.loading@gmail.com>
2025-04-01 13:57:15 +0700

    feature: add plugin options (#1)
    - Implement options parsing for the plugin
    - Add support for paths=source_relative option
    - Add support for output_prefix option
    - Create unit tests for options functionality
    - Add documentation for available options

--- go.mod
+++ go.mod
@@ -7,13 +7,4 @@ require (
 	google.golang.org/genproto/googleapis/api v0.0.0-20250324211829-b45e905df463
 )
 
-require (
-	github.com/davecgh/go-spew v1.1.1 // indirect
-	github.com/pmezard/go-difflib v1.0.0 // indirect
-	gopkg.in/yaml.v3 v3.0.1 // indirect
-)
-
-require (
-	github.com/stretchr/testify v1.10.0
-	google.golang.org/protobuf v1.36.6
-)
+require google.golang.org/protobuf v1.36.6
---
+++ httpinterface/options.go
@@ -0,0 +1,52 @@
+package httpinterface
+
+import (
+	"fmt"
+	"strings"
+)
+
+// Options represents the plugin options
+type Options struct {
+	// PathsSourceRelative determines if the output files should use source-relative paths
+	PathsSourceRelative bool
+	// OutputPrefix is an optional prefix for output files
+	OutputPrefix string
+	// Other options can be added here as needed
+}
+
+// ParseOptions parses the parameter string from protoc into an Options struct
+func ParseOptions(parameter string) (*Options, error) {
+	options := &Options{}
+
+	if parameter == "" {
+		return options, nil
+	}
+
+	params := strings.Split(parameter, ",")
+	for _, p := range params {
+		kv := strings.SplitN(p, "=", 2)
+		if len(kv) != 2 {
+			return nil, fmt.Errorf("invalid parameter: %s", p)
+		}
+
+		key := strings.TrimSpace(kv[0])
+		value := strings.TrimSpace(kv[1])
+
+		switch key {
+		case "paths":
+			if value == "source_relative" {
+				options.PathsSourceRelative = true
+			} else if value != "import" {
+				return nil, fmt.Errorf("unknown paths option: %s", value)
+			}
+		case "output_prefix":
+			options.OutputPrefix = value
+		default:
+			// You might want to either error on unknown options, or just ignore them
+			// For now, we'll log a warning and continue
+			fmt.Printf("Warning: unknown option: %s=%s\n", key, value)
+		}
+	}
+
+	return options, nil
+}
