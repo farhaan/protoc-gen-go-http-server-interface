cff29958ee08be9ad003ad65f562f12b71e1841e
farhaan <farhan.loading@gmail.com>
2025-05-13 16:59:53 +0700

    feat: add support for `editions` in HTTP interface generator and parser

--- httpinterface/annotations.go
+++ httpinterface/annotations.go
@@ -3,15 +3,30 @@ package httpinterface
 import (
 	"regexp"
 
+	"github.com/farhaan/protoc-gen-go-http-server-interface/httpinterface/parser"
 	options "google.golang.org/genproto/googleapis/api/annotations"
 	"google.golang.org/protobuf/proto"
 	descriptor "google.golang.org/protobuf/types/descriptorpb"
 )
 
+// HTTPRule represents an HTTP binding from annotations.
+type HTTPRule struct {
+	Method     string
+	Pattern    string
+	Body       string
+	PathParams []string
+}
+
 // HTTPRuleExtractor is the function type for extracting HTTP rules
 type HTTPRuleExtractor func(method *descriptor.MethodDescriptorProto) []HTTPRule
 
-// parseMethodHTTPRules extracts HTTP rules from a method descriptor
+// PathParamExtractor is the function type for extracting path parameters
+type PathParamExtractor func(pattern string) []string
+
+// PathPatternConverter is the function type for converting path patterns
+type PathPatternConverter func(pattern string) string
+
+// parseMethodHTTPRules extracts HTTP rules from a method descriptor using legacy approach
 func parseMethodHTTPRules(method *descriptor.MethodDescriptorProto) []HTTPRule {
 	rules := []HTTPRule{}
 
@@ -22,6 +37,7 @@ func parseMethodHTTPRules(method *descriptor.MethodDescriptorProto) []HTTPRule {
 			// Add the main rule
 			rule := parseHTTPRule(httpRule)
 			if rule.Method != "" {
+				rule.PathParams = parsePathParams(rule.Pattern)
 				rules = append(rules, rule)
 			}
 
@@ -29,16 +45,15 @@ func parseMethodHTTPRules(method *descriptor.MethodDescriptorProto) []HTTPRule {
 			for _, binding := range httpRule.AdditionalBindings {
 				rule := parseHTTPRule(binding)
 				if rule.Method != "" {
+					rule.PathParams = parsePathParams(rule.Pattern)
 					rules = append(rules, rule)
 				}
 			}
-
 		}
 	}
 
 	return rules
 }
-
 
 // parseHTTPRule extracts method, pattern, and body from an HttpRule
 func parseHTTPRule(httpRule *options.HttpRule) HTTPRule {
@@ -70,10 +85,7 @@ func parseHTTPRule(httpRule *options.HttpRule) HTTPRule {
 	return rule
 }
 
-// PathParamExtractor is the function type for extracting path parameters
-type PathParamExtractor func(pattern string) []string
-
-// parsePathParams extracts path parameters from a URL pattern
+// parsePathParams extracts path parameters from a URL pattern using regex
 func parsePathParams(pattern string) []string {
 	params := []string{}
 	re := regexp.MustCompile(`\{([^/{}]+)\}`)
@@ -88,12 +100,54 @@ func parsePathParams(pattern string) []string {
 	return params
 }
 
-
-// PathPatternConverter is the function type for converting path patterns
-type PathPatternConverter func(pattern string) string
-
 // convertPathPattern converts a path pattern to Go format
 func convertPathPattern(pattern string) string {
 	return pattern
 }
 
+// CreateHTTPRuleExtractorForFile creates an HTTP rule extractor for a specific file
+// This allows for editions support while maintaining dependency injection
+func CreateHTTPRuleExtractorForFile(file *descriptor.FileDescriptorProto) HTTPRuleExtractor {
+	return func(method *descriptor.MethodDescriptorProto) []HTTPRule {
+		// Create a parser for the current file
+		p := parser.CreateParser(file)
+
+		// Get rules from the parser
+		parsingRules := p.ParseHTTPRules(method)
+
+		// Convert to our HTTPRule type
+		rules := make([]HTTPRule, len(parsingRules))
+		for i, rule := range parsingRules {
+			rules[i] = HTTPRule{
+				Method:     rule.Method,
+				Pattern:    rule.Pattern,
+				Body:       rule.Body,
+				PathParams: rule.PathParams,
+			}
+		}
+
+		return rules
+	}
+}
+
+// CreatePathParamExtractorForFile creates a path parameter extractor for a specific file
+func CreatePathParamExtractorForFile(file *descriptor.FileDescriptorProto) PathParamExtractor {
+	return func(pattern string) []string {
+		// Create a parser for the current file
+		p := parser.CreateParser(file)
+
+		// Get path params from the parser
+		return p.ParsePathParams(pattern)
+	}
+}
+
+// CreatePathPatternConverterForFile creates a path pattern converter for a specific file
+func CreatePathPatternConverterForFile(file *descriptor.FileDescriptorProto) PathPatternConverter {
+	return func(pattern string) string {
+		// Create a parser for the current file
+		p := parser.CreateParser(file)
+
+		// Convert pattern using the parser
+		return p.ConvertPathPattern(pattern)
+	}
+}
---
+++ httpinterface/parser/parser.go
@@ -0,0 +1,61 @@
+package parser
+
+import (
+	descriptor "google.golang.org/protobuf/types/descriptorpb"
+)
+
+// HTTPRule represents an HTTP binding from annotations
+type HTTPRule struct {
+	Method     string
+	Pattern    string
+	Body       string
+	PathParams []string
+}
+
+type Parser interface {
+	// ParseHTTPRules extracts HTTP rules from a method descriptor
+	ParseHTTPRules(method *descriptor.MethodDescriptorProto) []HTTPRule
+
+	// ParsePathParams extracts path parameters from a URL pattern
+	ParsePathParams(pattern string) []string
+
+	// ConvertPathPattern converts a path pattern to Go format
+	ConvertPathPattern(pattern string) string
+}
+
+// CreateParser creates a parser appropriate for the given FileDescriptorProto
+func CreateParser(file *descriptor.FileDescriptorProto) Parser {
+	// Check for edition option
+	if hasEditionOption(file) {
+		return NewEditionsParser()
+	}
+
+	// Check syntax field
+	syntax := file.GetSyntax()
+	if syntax == "proto3" {
+		return NewProto3Parser()
+	}
+
+	// Default to proto2
+	return NewProto2Parser()
+}
+
+// hasEditionOption checks if a file has the edition option set
+func hasEditionOption(file *descriptor.FileDescriptorProto) bool {
+	if file.Options == nil {
+		return false
+	}
+
+	// This would need to be implemented to check for the edition option
+	// in the uninterpreted options of the file
+	// Check for edition in uninterpreted options
+	for _, option := range file.Options.UninterpretedOption {
+		for _, name := range option.Name {
+			if name.GetNamePart() == "edition" {
+				return true
+			}
+		}
+	}
+
+	return false
+}
